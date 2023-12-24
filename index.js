d9dc8ecb673f2e7560174c8d5ba0f39dfab8d7c7
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2023-12-24 20:49:48 +0100

    [add] response

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const fs = require("fs/promises");
-const express = require("express");
-const cors = require("cors");
-const _ = require("lodash");
-const { v4: uuid } = require("uiid");
+const fs = require('fs/promises');
+const express = require('express');
+const cors = require('cors');
+const _ = require('lodash');
+const { v4: uuid } = require('uuid');
+
+const app = express();
+const port = 3000;
+
+app.get('/token', (req, res) => {
+	res.send('<h1>token</h1>');
+});
+
+app.listen(port, () => console.log(`The SYS-TokenGenerator server runs on port ${port}`));

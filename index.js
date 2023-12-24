ceb74762c6db71407938d7642c50a0b3fabe2777
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2023-12-24 22:00:30 +0100

    [fix] post

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const fs = require('fs/promises');
 const express = require('express');
 const cors = require('cors');
-const _ = require('lodash');
-const { v4: uuid } = require('uuid');
 
 const app = express();
 const port = 3000;
 
-app.get('/token', (req, res) => {
-	res.send('<h1>token</h1>');
+app.use(express.json());
+app.use(cors());
+
+app.post('', (req, res) => {
+	const content = req.body;
+	console.log(content);
 });
 
 app.listen(port, () => console.log(`The SYS-TokenGenerator server runs on port ${port}`));

980c7fd92f87dc0ff10d48a3bf3bd362f9454656
Dwesh163 Desktop <ehouarn.duriaux@protonmail.com>
2023-12-26 18:02:49 +0100

    [add] fetch

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
+const SYS_SendMail_Port = 3003;
+
 app.use(express.json());
 app.use(cors());
 
 app.post('', (req, res) => {
 	const content = req.body;
 	console.log(content);
+
+	fetch(`http://localhost:${SYS_SendMail_Port}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			email: req.body.email,
+			token: 'hfse9f839r82r-ef89wgwgwg-fw9eg9we-3457384579',
+		}),
+	});
 });
 
 app.listen(port, () => console.log(`The SYS-TokenGenerator server runs on port ${port}`));

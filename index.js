7ccc01d7454ebc68a4b76df6c20fd72036145fae
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-01-06 12:07:05 +0100

    [fix] status

--- index.js
+++ index.js
@@ -12,22 +12,21 @@ app.use(cors());
 app.post('', (req, res) => {
 	const content = req.body;
 	console.log(content);
+	fetch(`http://192.168.0.97:${SYS_SendMail_Port}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			email: req.body.email,
+			type: 'token',
+			text: 'voici votre token : hfse9f839r82r-ef89wgwgwg-fw9eg9we-3457384579',
+		}),
+	});
+});
 
-	if (content.type == 'status') {
-		res.sendStatus(200);
-	} else {
-		fetch(`http://192.168.0.97:${SYS_SendMail_Port}`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				email: req.body.email,
-				type: 'token',
-				text: 'voici votre token : hfse9f839r82r-ef89wgwgwg-fw9eg9we-3457384579',
-			}),
-		});
-	}
+app.post('/status', (req, res) => {
+	res.sendStatus(200);
 });
 
 app.listen(port, () => console.log(`The SYS-TokenGenerator server runs on port ${port}`));
